9b10240181629c69703fae13f46dd3e04515ea51
Trong <66195006+GalaxyTea1@users.noreply.github.com>
2020-12-23 21:34:58 +0700

    update fix

--- backend/DataObjects.py
+++ backend/DataObjects.py
@@ -51,6 +51,55 @@ class Customer:
             if con is not None:
                 con.close()
 
+    def get_by_id(self, customer: CustomerEntity):
+        con = None
+        try:
+            con = psycopg2.connect(user = self.ConnectionData['user'],
+                                password = self.ConnectionData['password'],
+                                host = self.ConnectionData['host'],
+                                port = self.ConnectionData['port'],
+                                database = self.ConnectionData['database'])
+            cur = con.cursor()
+            sql = "SELECT * FROM TblCustomers WHERE customerid=%s"
+            cur.execute(sql, (customer.CustomerID,))
+            con.commit()           
+            row = cur.fetchone()
+            if row:
+                c = CustomerEntity()
+                c.fetch_data(row)
+                return c, 200
+            con.close()
+            return 'Customer ID not found', 404
+        except (Exception, psycopg2.DatabaseError) as error:
+            return str(error)
+        finally:
+            if con is not None:
+                con.close()
+
+
+    def update(self, customer: CustomerEntity):
+        con = None
+        try:
+            con = psycopg2.connect(user = self.ConnectionData['user'],
+                                password = self.ConnectionData['password'],
+                                host = self.ConnectionData['host'],
+                                port = self.ConnectionData['port'],
+                                database = self.ConnectionData['database'])
+            cur = con.cursor()
+            sql = "UPDATE TblCustomers SET customername=%s, contactname=%s, address=%s, city=%s, postalcode=%s, country=%s  WHERE customerid=%s"
+            cur.execute(sql, (customer.CustomerID, customer.ContactName, customer.Address, customer.City, customer.PostalCode, customer.Country, customer.CustomerID))
+            con.commit()           
+            row = cur.rowcount
+            if row > 0:
+               return 'Updated customer', 200
+            con.close()
+            return 'Customer ID not found', 404
+        except (Exception, psycopg2.DatabaseError) as error:
+            return str(error)
+        finally:
+            if con is not None:
+                con.close()
+
 
 if __name__ == "__main__":
     print('this is data object package')
